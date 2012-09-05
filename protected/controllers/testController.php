58b086b01be444eec9e860f93dff7701b52181b6
Usbuild <njuzhangqichao@gmail.com>
2012-09-05 21:44:37 +0800

    add .gitignore

--- protected/controllers/testController.php
+++ protected/controllers/testController.php
@@ -1,6 +1,9 @@
 <?php
 class TestController extends Controller {
     public function indexAction(){
+        $test = new Test();
+        $data = $test->findAllByAttributes(array());
+        var_dump($data);
         $this->render("index.php", array());
     }
 }
